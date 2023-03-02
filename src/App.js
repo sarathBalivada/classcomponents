e91da09d74fb334cdd2025d0ff22865e611ec2a0
Sarath Balivada <sarath.balivada95@gmail.com>
2023-03-02 12:39:34 +0530

    updates done

--- src/App.js
+++ src/App.js
@@ -8,17 +8,31 @@ export default class App extends React.Component {
 
         this.state = {
             mount: true,
+            ignoreProp: 0,
+            seed: 30,
+            showErrorComponenet: false,
         }
         this.mountCounter = () => this.setState({mount: true});
         this.unMountCounter = () => this.setState({mount: false});
-
+        
+        this.ignoreProp = () => this.setState({ ignoreProp: Math.random() });
+        this.seedGenerator = () => this.setState({ seed: Number.parseInt(Math.random() * 100) });
+        this.toggleError = () => this.setState({ showErrorComponenet: !this.state.showErrorComponenet });
     }
    render() {
     return (
         <div>
-            <button onClick={() => this.mountCounter()} disabled={this.state.mount}>mount</button>
-            <button onClick={() => this.unMountCounter()} disabled={!this.state.mount}>Un Mount</button>
-           { this.state.mount ? <Counter /> : ""}
+            <button onClick={this.mountCounter} disabled={this.state.mount}>mount</button>
+            <button onClick={this.unMountCounter} disabled={!this.state.mount}>Un Mount</button>
+            <button onClick={this.ignoreProp}>Ignore Prop</button>
+            <button onClick={this.seedGenerator}>Generate Seed</button>
+            <button onClick={this.toggleError}>Toggle error</button>
+           { this.state.mount ? 
+           <Counter
+           ignoreProp={this.state.ignoreProp} 
+           seed={this.state.seed}
+           showErrorComponenet={this.state.showErrorComponenet} /> : 
+           null }
         </div>
     )
    }
--- src/counter.js
+++ src/counter.js
@@ -1,42 +1,84 @@
-import React from 'react';  
-import ReactDom from 'react-dom/client';
+import React from "react";
+
+
+const ErrorComponent = () => <div>{props.ignore}</div>
 
 export default class Counter extends React.Component {
-   
-constructor(props) {
-    console.log('constructor')
-    super(props) ;
+  constructor(props) {
+    console.log("constructor");
+    super(props);
     this.state = {
-        counter: 0,
+      counter: 0,
+      seed: 0,
+    };
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed,
+      };
     }
-        this.increment = () => this.setState({counter: this.state.counter + 1});
-        this.decrement = () =>  this.setState({counter: this.state.counter - 1});
+    return null;
+  }
 
-}
-    componentDidMount() {
-        console.log("component did mount");
-        console.log("---------------");
+  componentDidMount() {
+    console.log("component did mount");
+    console.log("---------------");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("should component update - DO NOT RENDER");
+      console.log("---------------");
+      return false;
     }
+    console.log("should component update - RENDER");
+    console.log("---------------");
+    return true;
+  }
 
-    componentDidUpdate(prevProps,prevState,snapShot) {
-        console.log("component did update");
-        console.log("---------------"); 
-    }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+
+    console.log("Get snapshot before update");
+    return null;
+  }
+
+  render() {
+    console.log("render");
+        if (this.props.showErrorComponent &&  this.state.error) {
+            console.log('error', this.state.error.message)
+            return <div>We have encountered an error {this.state.error.message}</div>
+        }
+        
+    return (
+      <div>
+        <button onClick={() => this.increment()}>Increase</button>
+        <button onClick={() => this.decrement()}>Decrease</button>
+        <div className="counter">counter : {this.state.counter}</div>
+       {this.props.showErrorComponent ? <ErrorComponent /> : null}
+      </div>
+    );
+  }
+  componentDidUpdate(prevProps, prevState, snapShot) {
+    console.log("component did update");
+    console.log("---------------");
+  }
 
 
-render() {
-    console.log('render');
+  componentWillUnmount() {
+    console.log("component will unmount");
+    console.log("---------------");
+  }
 
-return (
-  <div>
-    <button onClick={() => this.increment()}>Increase</button>
-    <button onClick={() => this.decrement()}>Decrease</button>
-    <div className='counter'>
-    counter : {this.state.counter}
-    </div>
- 
-  </div>
-)
+  componentDidCatch(error, info) {
+    console.log('component Did Catch')
+    this.setState({ error,info })
+  }
 }
-}
-
